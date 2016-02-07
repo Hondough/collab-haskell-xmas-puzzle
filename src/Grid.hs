a96873158ae5dbaf99991a14fd32f64dd8559df9
John Krupka <john.r.krupka@gmail.com>
2016-02-07 12:05:04 -0600

    add Grid module

---
+++ src/Grid.hs
@@ -0,0 +1,97 @@
+module Grid (
+  Square(B,W)
+  ,drawLine
+  ,mkSquares
+  ,inc
+  ,rows
+  ,cols
+) where
+
+import qualified Data.List as L
+
+-- a Square can be either black or white
+data Square = B | W
+
+-- make it easier to see
+instance Show Square where
+  show B = "*"
+  show W = "."
+
+instance Eq Square where
+  B == B = True
+  W == W = True
+  _ == _ = False
+
+-- just a helper to simplify code a bit
+type Line = [Square]
+
+drawLine :: Line -> String
+drawLine = foldl (\t v -> t ++ show v) ""
+
+-- turn a list of runs into a list of Squares
+-- a run is an unbroken sequence of black Squares
+mkSquares :: [Int] -> [Square]
+mkSquares [] = []
+mkSquares (x:y:xs) = replicate x B ++ [W] ++ mkSquares (y:xs)
+mkSquares [x] = replicate x B
+
+-- grow a line by a single white block
+inc :: Line -> [Line]
+inc xs = [W : xs, fill xs, xs ++ [W]] where
+  fill xs = undefined --foldl (\t v -> if v == W) [] xs
+
+rows :: [[Int]]
+rows = [
+  [7,3,1,1,7],
+  [1,1,2,2,1,1],
+  [1,3,1,3,1,1,3,1],
+  [1,3,1,1,6,1,3,1],
+  [1,3,1,5,2,1,3,1],
+  [1,1,2,1,1],
+  [7,1,1,1,1,1,7],
+  [3,3],
+  [1,2,3,1,1,3,1,1,2],
+  [1,1,3,2,1,1],
+  [4,1,4,2,1,2],
+  [1,1,1,1,1,4,1,3],
+  [2,1,1,1,2,5],
+  [3,2,2,6,3,1],
+  [1,9,1,1,2,1],
+  [2,1,2,2,3,1],
+  [3,1,1,1,1,5,1],
+  [1,2,2,5],
+  [7,1,2,1,1,1,3],
+  [1,1,2,1,2,2,1],
+  [1,3,1,4,5,1],
+  [1,3,1,3,10,2],
+  [1,3,1,1,6,6],
+  [1,1,2,1,1,2],
+  [7,2,1,2,5]]
+
+cols :: [[Int]]
+cols = [
+  [7,2,1,1,7],
+  [1,1,2,2,1,1],
+  [1,3,1,3,1,3,1,3,1],
+  [1,3,1,1,5,1,3,1],
+  [1,3,1,1,4,1,3,1],
+  [1,1,1,2,1,1],
+  [7,1,1,1,1,1,7],
+  [1,1,3],
+  [2,1,2,1,8,2,1],
+  [2,2,1,2,1,1,1,2],
+  [1,7,3,2,1],
+  [1,2,3,1,1,1,1,1],
+  [4,1,1,2,6],
+  [3,3,1,1,1,3,1],
+  [1,2,5,2,2],
+  [2,2,1,1,1,1,1,2,1],
+  [1,3,3,2,1,8,1],
+  [6,2,1],
+  [7,1,4,1,1,3],
+  [1,1,1,1,4],
+  [1,3,1,3,7,1],
+  [1,3,1,1,1,2,1,1,4],
+  [1,3,1,4,3,3],
+  [1,1,2,2,2,6,1],
+  [7,1,3,2,1,1]]
