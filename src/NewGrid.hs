815d94485dbd8f4c55a384ed4c5aaf0751b65d44
John Krupka <john.krupka@united.com>
2016-02-24 13:20:10 -0600

    upgrade cabal helper, add start of the new solution converted from Python

---
+++ src/NewGrid.hs
@@ -0,0 +1,19 @@
+module NewGrid where
+
+data Block = B | W
+
+type Grid = [[Block]]
+
+instance Show Block where
+  show B = "1"
+  show W = "_"
+
+instance Eq Block where
+  B == B = True
+  W == W = True
+  _ == _ = False
+
+instance Ord Block where
+  compare B W = GT
+  compare W B = LT
+  compare _ _ = EQ
