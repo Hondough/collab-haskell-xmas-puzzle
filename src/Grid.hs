6ab67f365a33748ba80c1204a918212a76e4459f
John Krupka <john.r.krupka@gmail.com>
2016-03-27 14:09:21 -0500

    work on expand and expandRun

--- src/Grid.hs
+++ src/Grid.hs
@@ -9,7 +9,7 @@ data Block = B | W | U
 type Row = Int
 type Col = Int
 type Offset = Int
-type Runs = [Int]
+type Run = Int
 type Line = V.Vector Block
 type Grid = V.Vector Line --[[Block]]
 
@@ -44,7 +44,7 @@ instance Eq LineData where
 instance Ord LineData where
   compare a b = compare (moves a) (moves b)
 
-mkLineData :: LineDir -> Int -> Runs -> LineData
+mkLineData :: LineDir -> Int -> [Run] -> LineData
 mkLineData direction index runs = LineData {
   dir = direction
   ,idx = index
@@ -84,19 +84,16 @@ fillCol :: Col -> Line -> Grid -> Grid
 fillCol col line grid = foldr (\v acc -> writeBlock (snd v) (fst v) col acc) grid coords where
   coords = zip [0..] (V.toList line)
 
-checkLine :: Line -> LineDir -> Offset -> Grid -> Bool
-checkLine line dir offset grid = undefined
-
 -- turn a list of runs into a Line (list of Blocks)
 -- a "run" is an unbroken sequence of black Blocks
-mkLine :: Runs -> Line
+mkLine :: [Run] -> Line
 mkLine [] = V.empty
 mkLine [x] = run x B
 mkLine (x:xs) = run x B V.++ V.singleton W V.++ mkLine xs
 
 -- returns the number of free spaces we have to move blocks around within
 -- within the max length of a line corresponding to the Int list
-freeSpaces :: Int -> Runs -> Int
+freeSpaces :: Int -> [Run] -> Int
 freeSpaces maxLen runs = if len < 0 then 0 else len where
   len = maxLen - lineLen
   lineLen = sum runs + length runs - 1
@@ -104,28 +101,15 @@ freeSpaces maxLen runs = if len < 0 then 0 else len where
 run :: Int -> Block -> Line
 run = V.replicate
 
-grow :: Int -> Line -> V.Vector Line
-grow moves line
-  | moves <= 0 = V.singleton line
-  | otherwise = line `V.cons` grow (moves - 1) (V.cons W line)
+curry3 :: (a -> b -> c -> d) -> (a,b,c) -> d
+curry3 f (a,b,c) = f a b c
 
-growRuns :: Int -> Runs -> V.Vector Line
-growRuns maxlen xs = V.concatMap (grow moves) lines where
-  moves = freeSpaces maxlen xs
-  lines = V.map (mkLine . (:[])) (V.fromList xs)
+-- close but not quite right
+expand :: (Line, [Run], Int) -> [(Line, [Run], Int)]
+expand (line, [], 0) = [(line, [], 0)]
+expand (line, [], free) = [(line V.++ run free W, [], 0)]
+expand (line, x:xs, free) = concatMap (expand . foo line) (expandRun x free) where
+  foo oldLine (line, free) = (oldLine V.++ V.singleton W V.++ line, xs, free)
 
-growRuns' :: Int -> Runs -> V.Vector (V.Vector Line)
-growRuns' maxlen xs = V.map (grow moves) lines where
-  moves = freeSpaces maxlen xs
-  lines = V.map (mkLine . (:[])) (V.fromList xs)
-
-expand :: Line -> Runs -> Int -> [(Line, Runs, Int)]
-expand line (x:xs) freeSpaces =
-  [(run n W V.++ run x B V.++ add line, xs, freeSpaces - n) |
-    n <- [0..freeSpaces]] where
-      add line | line == V.empty = V.empty
-               | V.head line == B = V.cons W line
-               | otherwise = line
-
--- the basic idea
--- concatMap (\(a,b,c) -> expand a b c) $ expand (V.fromList [W,B,B]) [3,2] 2
+expandRun :: Run -> Int -> [(Line, Int)]
+expandRun r free = [(run n W V.++ run r B, free - n) | n <- [0..free]]
